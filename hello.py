a4194cda71317d37baf18340048bfa74823cee22
jen <jenhung.jocker@gmail.com>
2022-07-12 16:27:18 +0800

    try to deploy

--- hello.py
+++ hello.py
@@ -45,4 +45,8 @@ def colors(palette):
 
     return jsonify(result)
 
-app.run(debug=True)
+# app.run(debug=True)
+
+if __name__  == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0",port=5777)
