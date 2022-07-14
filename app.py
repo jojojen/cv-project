d5c98af84d4643310d65e0d3538d966f408080f9
jen <jenhung.jocker@gmail.com>
2022-07-14 23:47:10 +0800

    try

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, redirect
+from flask import Flask, jsonify, redirect, request
 from flasgger import Swagger
 
 app = Flask(__name__)
@@ -8,23 +8,24 @@ swagger = Swagger(app)
 def api_index():
     return redirect('/apidocs/')
 
-@app.route('/v1/img_for_word', methods=['GET'])
+@app.route('/v1/img_for_word/', methods=['GET'])
 def get_img_for_word():
-  """
-    Get img for word
-    ---
-    tags:
-      - Node APIs
-    produces: application/json,
-    parameters:
-    - name: word
-      in: query
-      type: string
-      required: true
-    responses:
-      200:
-        description: Retrieve image for word
-        examples:
-          ans: {"main_color":"red", "main_shape":["00100", "01010", "10001", "00100", "01010"]}
-  """
-  return jsonify(ans={"main_color":"red", "main_shape":["00100", "01010", "10001", "01010", "00100"]})
+    """
+      Get img for word
+      ---
+      tags:
+        - Computer Vision APIs
+      produces: application/json,
+      parameters:
+      - name: word
+        in: query
+        type: string
+        required: true
+      responses:
+        200:
+          description: Retrieve image for word
+          examples:
+            ans: {"main_color":"red", "main_shape":["00100", "01010", "10001", "00100", "01010"]}
+    """
+    word = request.args.get('word', 1)
+    return jsonify(ans={"main_color":"red", "main_shape":["00100", "01010", word, "01010", "00100"]})
