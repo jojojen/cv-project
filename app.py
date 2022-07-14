3815c95f2907181de521900bc4d3032aac58c045
jen <jenhung.jocker@gmail.com>
2022-07-14 22:43:39 +0800

    try simple

--- app.py
+++ app.py
@@ -24,7 +24,3 @@ def test_api():
           ans: {"main_color":"red", "main_shape":["00100", "01010", "10001", "00100", "01010"]}
   """
   return jsonify(ans={"main_color":"red", "main_shape":["00100", "01010", "10001", "01010", "00100"]})
-
-if __name__  == '__main__':
-    app.debug = True
-    app.run()
